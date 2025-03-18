e81f436b0134b95aea68412e27c610608b8180cd
Sarven Capadisli <info@csarven.ca>
2025-03-18 16:39:40 +0100

    Update slashmenu handlers
    Co-authored-by: Virginia Balseiro <info@virginiabalseiro.com>

--- src/editor/slashmenu/handlers.js
+++ src/editor/slashmenu/handlers.js
@@ -1,39 +1,76 @@
-import { getFormValues } from "../../util.js";
+import { createResourceTypeHTML, createLanguageHTML, createLicenseHTML, createInboxHTML, createInReplyToHTML } from "../../doc.js";
+import { getFormValues, fragmentFromString } from "../../util.js";
 
 export function formHandlerLanguage(e) {
   e.preventDefault();
   e.stopPropagation();
 
   const formValues = getFormValues(e.target);
-  console.log(formValues);
+  // console.log(formValues);
   const language = formValues['language'];
+  const options = {};
 
+  const htmlString = createLanguageHTML(language, options);
 
-  console.log("language handler")
+  this.replaceSelectionWithFragment(fragmentFromString(htmlString));
+  this.hideMenu()
 }
 
-export function formHandlerLicense() {
-    
+export function formHandlerLicense(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  const formValues = getFormValues(e.target);
+  // console.log(formValues);
+  const license = formValues['license'];
+  const options = {};
+
+  const htmlString = createLicenseHTML(license, options);
+
+  this.replaceSelectionWithFragment(fragmentFromString(htmlString));
 }
 
-export function formHandlerInbox() {
-//process form 
+export function formHandlerInbox(e) {
+  e.preventDefault();
+  e.stopPropagation();
 
-// assemble htmlString
+  const formValues = getFormValues(e.target);
+  // console.log(formValues);
+  const inbox = formValues['inbox'];
+  const options = {};
 
-// create fragment
+  const htmlString = createInboxHTML(inbox, options);
 
-// submitButton.addEventListener("click", () => {
-  //   this.replaceSelectionWithFragment(fragment);
-  //   this.hideMenu();
-  // });
-
+  this.replaceSelectionWithFragment(fragmentFromString(htmlString));
+  this.hideMenu()
 }
 
-export function formHandlerInReplyTo() {
-    
+export function formHandlerInReplyTo(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  const formValues = getFormValues(e.target);
+  // console.log(formValues);
+  const inReplyTo = formValues['in-reply-to'];
+  const options = {};
+
+  const htmlString = createInReplyToHTML(inReplyTo, options);
+
+  this.replaceSelectionWithFragment(fragmentFromString(htmlString));
+  this.hideMenu()
 }
 
-export function formHandlerDocumentType() {
+export function formHandlerResourceType(e) {
+  e.preventDefault();
+  e.stopPropagation();
 
+  const formValues = getFormValues(e.target);
+  // console.log(formValues);
+  const resourceType = formValues['resource-type'];
+  const options = {};
+
+  const htmlString = createResourceTypeHTML(resourceType, options);
+
+  this.replaceSelectionWithFragment(fragmentFromString(htmlString));
+  this.hideMenu()
 }
