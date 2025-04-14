374c1996db6ba8918425dab6328e88963bdfa976
Virginia Balseiro <info@virginiabalseiro.com>
2025-04-14 10:25:16 +0200

    Improvements to mock fetch

--- tests/utils/mockFetch.js
+++ tests/utils/mockFetch.js
@@ -1,11 +1,34 @@
+import { jest } from "@jest/globals";
+
 export const mockFetch = jest.fn();
 
 export const mockFetchHandler = (responses = {}) => {
-  return (url, options) => {
+  return (input, options) => {
+    const url = typeof input === "string" ? input : input.url;
+
+    console.warn("Fetching:", url);
+
+    if (responses[url] && responses[url].ok === false) {
+      const error = new Error(
+        `Error fetching resource: ${responses[url].status} ${responses[url].statusText}`
+      );
+      error.status = responses[url].status;
+      error.response = responses[url];
+
+      return Promise.reject(new Error(error));
+    }
+    
     if (responses[url]) {
-      return Promise.resolve(responses[url]);
+      return Promise.resolve({
+        ok: true,
+        status: 200,
+        statusText: "OK",
+        json: () => Promise.resolve({ data: responses[url].data }),
+        text: () => Promise.resolve(responses[url].data),
+      });    
     }
 
+    console.error(`Unhandled fetch: ${url}`);
     return Promise.reject(new Error(`Unhandled fetch to ${url}`));
   };
 };
