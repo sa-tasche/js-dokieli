6717dfd52276a2471a640cbf961e5e28b5d93d68
Virginia Balseiro <info@virginiabalseiro.com>
2025-04-23 20:27:17 +0200

    e2e tests: update auth flow tests

--- tests/e2e/browser/login.spec.js
+++ tests/e2e/browser/login.spec.js
@@ -1,25 +1,22 @@
 import { test, expect } from "./fixtures";
+test.describe("auth flow", () => {
+  test.beforeEach(async ({ auth }) => {
+    await auth.login();
+  });
 
-test.skip("Skipping the entire file temporarily because it relies on authn which is currently broken due to breaking changes in the test server we are using");
-test.beforeEach(async ({ auth }) => {
-  await auth.login();
-});
+  test("signs in", async ({ page }) => {
+    await expect(page.locator("button.signout-user")).toBeVisible();
+  });
 
+  test("signs out", async ({ page }) => {
+    await page.waitForSelector('button.signout-user');
+    await expect(page.locator("button.signout-user")).toBeVisible();
 
-test("signs in", async ({ page }) => {
-  await expect(page.locator("button.signout-user")).toBeVisible();
-});
+    await page.waitForTimeout(1000);
+    await page.locator("button.signout-user").click();
 
-test("signs out", async ({ page }) => {
-
-  await page.waitForSelector('button.signout-user');
-  await expect(page.locator("button.signout-user")).toBeVisible();
-
-  // Waiting for sign in to be completed, otherwise it gets as it is still trying to retrieve data from WebID when sign out button is clicked
-  await page.waitForTimeout(1000);
-
-  await page.locator("button.signout-user").click();
-
-  await page.waitForSelector('button.signin-user');
-  await expect(page.locator("button.signin-user")).toBeVisible();
+    await page.waitForTimeout(1000);
+    await page.waitForSelector('button.signin-user');
+    await expect(page.locator("button.signin-user")).toBeVisible();
+  });
 });
