59a08ec14bc0b56075e405aedbf0a52cd5f6a579
Virginia Balseiro <info@virginiabalseiro.com>
2025-04-23 20:28:11 +0200

    e2e tests: update bookmark tests

--- tests/e2e/browser/bookmark.spec.js
+++ tests/e2e/browser/bookmark.spec.js
@@ -1,13 +1,26 @@
 import { test, expect } from "./fixtures";
-import { selectText } from "./utils";
 
-test.skip("Skipping the entire file temporarily because it relies on authn which is currently broken due to breaking changes in the test server we are using");
-
-test.beforeEach(async ({ auth }) => {
+test.beforeEach(async ({ auth, page }) => {
   await auth.login();
+  await page.waitForLoadState("load");
+
+  // Click and drag on text to select it
+  const text = page.locator("#summary");
+  const box = await text.boundingBox();
+
+  await text.click();
+  await page.mouse.down();
+  await page.mouse.move(box.x + 30, box.y + box.height / 2);
+  await page.mouse.up();
+
+  // Wait for the toolbar to be visible
+  const toolbar = page.locator(".editor-toolbar");
+  await expect(toolbar).toBeVisible();
 });
 
-async function cleanup(page) {
+async function cleanup(page, bookmark) {
+  bookmark.click();
+  await page.waitForTimeout(1000);
   // clean up created bookmark
   await page.locator("button.delete");
   await page.click("button.delete");
@@ -15,25 +28,27 @@ async function cleanup(page) {
 }
 
 test("should be able to bookmark a resource", async ({ page }) => {
-  await expect(page.locator("button.signout-user")).toBeVisible();
-  await page.getByRole("button", { name: "Hide Menu" }).click();
+  // TODO: we should not need this here, login should be enough
+  // Listen for console messages to make sure we are logged in 
+  await page.on("console", async (msg) => {
+    if (
+      msg.text().includes(process.env.WEBID)
+    ) {
+      const bookmarkButton = page.locator('[id="editor-button-bookmark"]');
+      await bookmarkButton.click();
+      await expect(page.locator("textarea#bookmark-content")).toBeVisible();
+      await page.fill("textarea#bookmark-content", "This is a bookmark");
+      const saveButton = page.getByRole("button", { name: "Post" });
+      expect(saveButton).toBeVisible();
+      await saveButton.click();
 
-  // FIXME: maybe move this to a fixture?
-  await selectText("annotations", page);
+      const bookmark = page.locator("sup.ref-annotation");
+      await expect(bookmark).toBeVisible();
 
-  const bookmarkButton = page.locator("[data-action=bookmark]");
-  await bookmarkButton.click();
-  await expect(page.locator("textarea#bookmark-content")).toBeVisible();
-  await page.fill("textarea#bookmark-content", "This is a bookmark");
-  const saveButton = page.locator(
-    "textarea#bookmark-content ~ a.medium-editor-toolbar-save"
-  );
-  expect(saveButton).toBeVisible();
-  await saveButton.click();
-  await expect(page.locator("sup.ref-annotation")).toBeVisible();
+      // wait for screencast purposes
+      // await page.waitForTimeout(5000);
 
-  //   // wait for screencast purposes
-  //   await page.waitForTimeout(5000);
-
-  await cleanup(page);
+      await cleanup(page, bookmark);
+    }
+  });
 });
