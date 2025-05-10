faee73791c8fb8143da4148b09be17b6638c2ee8
Sarven Capadisli <info@csarven.ca>
2025-05-10 17:24:46 +0200

    Add human-readable label text for feature-license-options

--- src/dokieli.js
+++ src/dokieli.js
@@ -1680,7 +1680,7 @@ DO = {
 
       var licenseOptions = document.querySelectorAll('[about="#feature-license-options"][typeof="schema:ChooseAction"], [href="#feature-license-options"][typeof="schema:ChooseAction"], [resource="#feature-license-options"][typeof="schema:ChooseAction"]');
       for (var i = 0; i < licenseOptions.length; i++){
-        licenseOptions[i].parentNode.replaceChild(fragmentFromString('<label class="do" for="feature-license-options"></label><select class="do" id="feature-license-options">' + getLicenseOptionsHTML() + '</select>'), licenseOptions[i]);
+        licenseOptions[i].parentNode.replaceChild(fragmentFromString('<label class="do" for="feature-license-options">License</label><select class="do" id="feature-license-options">' + getLicenseOptionsHTML() + '</select>'), licenseOptions[i]);
       }
 
       var languageOptions = document.querySelectorAll('[about="#feature-language-options"][typeof="schema:ChooseAction"], [href="#feature-language-options"][typeof="schema:ChooseAction"], [resource="#feature-language-options"][typeof="schema:ChooseAction"]');
