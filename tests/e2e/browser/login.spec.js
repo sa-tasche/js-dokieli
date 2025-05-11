cd9cd2b8111bb5ad53182a7ed50341e409454576
Virginia Balseiro <info@virginiabalseiro.com>
2025-05-11 14:53:10 +0200

    Improvements to login tests

--- tests/e2e/browser/login.spec.js
+++ tests/e2e/browser/login.spec.js
@@ -1,22 +1,33 @@
 import { test, expect } from "./fixtures";
 test.describe("auth flow", () => {
-  test.beforeEach(async ({ auth }) => {
+  test.beforeEach(async ({ auth, page }) => {
     await auth.login();
+    await page.locator("#document-menu button").click();
   });
 
   test("signs in", async ({ page }) => {
-    await expect(page.locator("button.signout-user")).toBeVisible();
+    // Listen for console messages to make sure we are logged in - FIXME: not sure why this is still needed at this point
+    page.on("console", async (msg) => {
+      if (msg.text().includes(process.env.WEBID)) {
+        await page.waitForSelector("#document-menu", { state: "visible" });
+        await expect(page.locator("button.signout-user")).toBeVisible();
+      }
+    });
   });
-
   test("signs out", async ({ page }) => {
-    await page.waitForSelector('button.signout-user');
-    await expect(page.locator("button.signout-user")).toBeVisible();
-
-    await page.waitForTimeout(1000);
-    await page.locator("button.signout-user").click();
-
-    await page.waitForTimeout(1000);
-    await page.waitForSelector('button.signin-user');
-    await expect(page.locator("button.signin-user")).toBeVisible();
+    // Listen for console messages to make sure we are logged in - FIXME: not sure why this is still needed at this point
+    page.on("console", async (msg) => {
+      if (msg.text().includes(process.env.WEBID)) {
+        await page.waitForSelector("button.signout-user");
+        await expect(page.locator("button.signout-user")).toBeVisible();
+    
+        await page.waitForTimeout(1000);
+        await page.locator("button.signout-user").click();
+    
+        await page.waitForTimeout(1000);
+        await page.waitForSelector("button.signin-user");
+        await expect(page.locator("button.signin-user")).toBeVisible();
+      }
+    });
   });
 });
