c2548a6222dced491d32cc7cb2b9d1f6faa1cbfb
Virginia Balseiro <info@virginiabalseiro.com>
2025-05-12 06:50:55 +0200

    Refactor graph and notifications tests

--- tests/e2e/browser/graph.spec.js
+++ tests/e2e/browser/graph.spec.js
@@ -1,46 +1,46 @@
 import { test, expect } from "@playwright/test";
 import AxeBuilder from "@axe-core/playwright";
 
-test("graph loads and passes accessibility checks", async ({ page }) => {
-  let graphModal;
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+  await page.waitForLoadState("load");
 
-  await test.step("load graph", async () => {
-    await page.goto("/");
-    await page.waitForLoadState("load");
+  await page.locator("#document-menu button").click();
+  const menu = page.locator("[id=document-menu]");
+  await expect(menu).toBeVisible();
 
-    await page.locator("#document-menu button").click();
-    const menu = page.locator("[id=document-menu]");
-    await expect(menu).toBeVisible();
+  const graphButton = page.locator("[class=resource-visualise]");
+  await graphButton.click();
+  const graphModal = page.locator("[id=graph-view]");
+  await expect(graphModal).toBeVisible();
+});
 
-    const graphButton = page.locator("[class=resource-visualise]");
-    await graphButton.click();
-    graphModal = page.locator("[id=graph-view]");
-    await expect(graphModal).toBeVisible();
-    const graphSvg = graphModal.locator('svg[typeof*="Image"]');
-    await expect(graphSvg).toBeVisible();
-  });
+test("graph loads", async ({ page }) => {
+  const graphModal = page.locator("[id=graph-view]");
+  const graphSvg = graphModal.locator('svg[typeof*="Image"]');
+  await expect(graphSvg).toBeVisible();
+});
 
-  await test.step("graph modal has no automatically detectable accessibility issues", async () => {
-    const accessibilityScanResults = await new AxeBuilder({ page })
-      .include(await graphModal.elementHandle())
-      .analyze();
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+test("graph modal has no automatically detectable accessibility issues", async ({ page }) => {
+  const graphModal = page.locator("[id=graph-view]");
+  const accessibilityScanResults = await new AxeBuilder({ page })
+    .include(await graphModal.elementHandle())
+    .analyze();
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
 
-  await test.step("graph modal has no WCAG A, AA, or AAA violations", async () => {
-    const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags([
-        "wcag2a", "wcag2aa", "wcag2aaa",
-        "wcag21a", "wcag21aa", "wcag21aaa"
-      ])
-      .include(await graphModal.elementHandle())
-      .analyze();
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
-
-  await test.step("close graph modal", async () => {
-    const closeButton = graphModal.locator(".close");
-    await closeButton.click();
-    await expect(graphModal).not.toBeVisible();
-  });
+test("graph modal has no WCAG A, AA, or AAA violations", async ({ page }) => {
+  const graphModal = page.locator("[id=graph-view]");
+  const accessibilityScanResults = await new AxeBuilder({ page })
+    .withTags([
+      "wcag2a",
+      "wcag2aa",
+      "wcag2aaa",
+      "wcag21a",
+      "wcag21aa",
+      "wcag21aaa",
+    ])
+    .include(await graphModal.elementHandle())
+    .analyze();
+  expect(accessibilityScanResults.violations).toEqual([]);
 });
