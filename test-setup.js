66934cc4ac4b5a9081ef945049808b9b6bbf0577
Virginia Balseiro <info@virginiabalseiro.com>
2025-05-20 20:32:30 +0200

    Fix crypto global in tests

--- test-setup.js
+++ test-setup.js
@@ -1,16 +1,16 @@
 import { jest } from '@jest/globals';
 import { TextEncoder, TextDecoder } from "util";
-import { webcrypto } from "crypto";
 import { mockFetch } from './tests/utils/mockFetch';
+import crypto from 'crypto';
+
+Object.defineProperty(globalThis, 'crypto', {
+  value: {
+    ...crypto,
+  }
+});
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
-
-Object.defineProperty(global.self, "crypto", {
-  value: {
-    subtle: webcrypto.subtle,
-  },
-});
 
 global.fetch = mockFetch;
 global.jest = jest;
